44b3648e33aa28b2c796b15cdfc6836962ff436d
Hugo Ribeira <hugoribeira@gmail.com>
2017-09-17 14:13:05 +0100

    Refactor tests to not depend on implementation details

--- lib/rex/application.ex
+++ lib/rex/application.ex
@@ -1,19 +1,10 @@
 defmodule Rex.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: Rex.Worker.start_link(arg)
-      # {Rex.Worker, arg},
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+    children = []
     opts = [strategy: :one_for_one, name: Rex.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- test/rex_test.exs
+++ test/rex_test.exs
@@ -1,6 +1,7 @@
 defmodule RexTest do
   use ExUnit.Case
-  doctest Rex
+
+  alias Rex.QueueManager
 
   defmodule TestJob do
     use Rex.Job
@@ -10,9 +11,80 @@ defmodule RexTest do
     end
   end
 
+  defmodule WaitToDie do
+    use Rex.Job
+
+    def perform(pid) do
+      send pid, {:ping, self()}
+      :timer.sleep(:infinity)
+    end
+  end
+
+  defmodule AnotherWaitToDie do
+    use Rex.Job
+
+    def perform(pid) do
+      send pid, {:ping, self()}
+      :timer.sleep(:infinity)
+    end
+  end
+
+  defmodule GroupedWaitToDie do
+    use Rex.Job
+
+    def group_by(key, _), do: key
+
+    def perform(key, pid) do
+      send pid, key
+      :timer.sleep(:infinity)
+    end
+  end
+
+  setup do
+    {:ok, _} = QueueManager.Supervisor.start_link
+
+    on_exit fn ->
+      Process.exit(Process.whereis(QueueManager.Supervisor), :kill)
+      :ok
+    end
+
+    :ok
+  end
+
   test "processes a job" do
-    {:ok, _} = Rex.QueueManager.Supervisor.start_link
     :ok = Rex.enqueue(TestJob, [self()])
     assert_receive :test_job_ack
+  end
+
+  describe "dispatching mechanism" do
+    test "runs jobs in parallel for the same queue" do
+      :ok = Rex.enqueue(WaitToDie, [self()])
+      assert_receive {:ping, pid1}
+
+      :ok = Rex.enqueue(WaitToDie, [self()])
+      assert_receive {:ping, pid2}
+      refute pid1 == pid2
+    end
+
+    test "runs jobs in parallel for different queues" do
+      :ok = Rex.enqueue(WaitToDie, [self()])
+      assert_receive {:ping, pid1}
+
+      :ok = Rex.enqueue(AnotherWaitToDie, [self()])
+      assert_receive {:ping, pid2}
+      refute pid1 == pid2
+    end
+
+    test "runs jobs synchronously for the same queue" do
+      :ok = Rex.enqueue(GroupedWaitToDie, ["one_key", self()])
+      assert_receive "one_key"
+
+      :ok = Rex.enqueue(GroupedWaitToDie, ["other_key", self()])
+      assert_receive "other_key"
+
+      # one_key is still sleeping, so the second "one_key" will never run.
+      :ok = Rex.enqueue(GroupedWaitToDie, ["one_key", self()])
+      refute_receive "one_key"
+    end
   end
 end
